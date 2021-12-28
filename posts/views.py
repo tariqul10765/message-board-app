b8f6c488b12bd1a9af86ef872d2a418c5044ab59
tariqul10765 <tariqul10765@gmail.com>
2021-12-28 23:49:42 +0600

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from posts.models import Post
+from django.views.generic import ListView
+
+# Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
